7e0722a13b4224bf2e7bffeb7c03b2ea746987cd
winkymonkey <37624113+winkymonkey@users.noreply.github.com>
2020-09-30 04:31:06 +0530

    Add files via upload

---
+++ src/main/java/com/example/spring/boot/MyCustomEndpoint.java
@@ -0,0 +1,32 @@
+package com.example.spring.boot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.actuate.endpoint.Endpoint;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MyCustomEndpoint implements Endpoint<List<String>> {
+     
+    public String getId() {
+        return "customEndpoint";
+    }
+ 
+    public boolean isEnabled() {
+        return true;
+    }
+ 
+    public boolean isSensitive() {
+        return true;
+    }
+ 
+    public List<String> invoke() {
+        // Custom logic to build the output
+        List<String> messages = new ArrayList<String>();
+        messages.add("This is message 1");
+        messages.add("This is message 2");
+        return messages;
+    }
+}
---
+++ src/main/java/com/example/spring/boot/MyHealthIndicator.java
@@ -0,0 +1,23 @@
+package com.example.spring.boot;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MyHealthIndicator implements HealthIndicator {
+
+	public Health health() {
+		int errorCode = check();
+		if (errorCode != 0) {
+			return Health.down().withDetail("Error Code", errorCode).build();
+		}
+		return Health.up().build();
+	}
+
+	public int check() {
+		// Our custom logic to check health
+		return 0;
+	}
+}
